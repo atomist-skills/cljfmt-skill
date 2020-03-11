1650ba6b5812de6cad6df2dc8774d878d9beddc6
Jim Clark <slimslenderslacks@gmail.com>
2020-03-11 00:01:11 -0700

    update skill to support both edit on branch and edit in PR policies

--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -5,24 +5,53 @@
             [atomist.cljs-log :as log]
             [atomist.api :as api]
             [atomist.cljfmt :as cljfmt]
+            [atomist.sdmprojectmodel :as sdm]
             ["@atomist/automation-client" :as ac]
-            [atomist.deps :as deps])
+            [atomist.deps :as deps]
+            [goog.string :as gstring]
+            [goog.string.format])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defn run-cljfmt
-  [request project]
+  [project]
   (go
    (try
+     ;; weird that cljfmt is synchronous
      (cljfmt/cljfmt (. ^js project -baseDir))
+     :done
      (catch :default ex
        (log/error "unable to run cljfmt")
        (log/error ex)
        {:error ex
         :message "unable to run cljfmt"}))))
 
+(defn compose-wrapper
+  [request project]
+  (go
+   (<! (((:wrapper request) run-cljfmt) project))))
+
+(defn- check-configuration [handler]
+  (fn [request]
+    (cond (= "inPR" (:policy request))
+          (handler (assoc request :wrapper (fn [cb] (sdm/edit-inside-PR cb {:branch (gstring/format "cljfmt-%s" (-> request :ref :branch))
+                                                                            :target-branch (-> request :ref :branch)
+                                                                            :body "cljfmt updates"
+                                                                            :title "cljfmt updates"}))))
+          (= "onBranch" (:policy request))
+          (handler (assoc request :wrapper (fn [cb] (sdm/commit-then-push cb "cljformat update"))))
+          :else
+          (api/finish request :failure "skill requires either 'update directly' or 'update in PR' to be configured"))))
+
 (defn- handle-push-event [request]
-  ((-> (api/finished :message "handling Push" :success "successfully handled Push event")
-       (api/run-sdm-project-callback run-cljfmt)
+  ((-> (api/finished :message "handling Push"
+                     :send-status (fn [request]
+                                    (cond (= :raised (-> request :results))
+                                          (gstring/format "**cljfmt skill** raised a PR")
+                                          :else
+                                          "handled Push successfully")))
+       (api/run-sdm-project-callback compose-wrapper)
+       (check-configuration)
+       (api/add-skill-config :policy)
        (api/extract-github-token)
        (api/create-ref-from-push-event)) request))
 
