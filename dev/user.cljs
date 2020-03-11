f470d9ee78a485bccec690a53b8a1ed7fdc8d1a6
Jim Clark <slimslenderslacks@gmail.com>
2020-03-11 13:34:26 -0700

    bump

--- dev/user.cljs
+++ dev/user.cljs
@@ -4,7 +4,6 @@
             [atomist.api :as api]
             [cljs.core.async :refer [<!]]
             [atomist.sdmprojectmodel :as sdm]
-            ["@atomist/sdm" :as atomistsdm]
             ["@atomist/automation-client" :as ac]
             ["@atomist/automation-client/lib/operations/support/editorUtils" :as editor-utils]
             [atomist.json :as json])
@@ -14,4 +13,28 @@
 
 (def token (.. js/process -env -API_KEY_SLIMSLENDERSLACKS_STAGING))
 (def github-token (.. js/process -env -GITHUB_TOKEN))
+
+(defn fake-handler [& args]
+  (log/info "args " args))
+
+(defn fake-push-on-clj1-in-staging [policy]
+ (.catch
+  (.then
+   (atomist.main/handler #js {:data {:Push [{:branch "master"
+                                             :repo {:name "clj1"
+                                                    :org {:owner "atomisthqa"
+                                                          :scmProvider {:providerId "zjlmxjzwhurspem"
+                                                                        :credential {:secret github-token}}}}
+                                             :after {:message ""}}]}
+                              :configuration {:name "clj-format"
+                                              :parameters [{:name "policy" :value policy}]}
+                              :secrets [{:uri "atomist://api-key" :value token}]
+                              :extensions [:team_id "AK748NQC5"]}
+                         fake-handler)
+   (fn [v] (log/info "value " v)))
+  (fn [error] (log/info "error " error))))
+
+(comment
+ (fake-push-on-clj1-in-staging "inPR")
+ (fake-push-on-clj1-in-staging "onBranch"))
 
