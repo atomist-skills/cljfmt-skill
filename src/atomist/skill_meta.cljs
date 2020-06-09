62bdca6ed5efe582dee0d57b0289d3927ac658f7
Jim Clark <slimslenderslacks@gmail.com>
2020-06-09 16:11:21 -0700

    Bump cljformat-skill to 0.1.8 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "cljformat-skill")
 (def path ".")
-(def version "0.1.7")
-(def tag "0.1.7")
-(def generated-at "2020-06-09T22:51:03Z")
+(def version "0.1.8")
+(def tag "0.1.8")
+(def generated-at "2020-06-09T23:11:21Z")
