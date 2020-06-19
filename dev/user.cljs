30eb460220d44d482480ec616fdcf99a6d2823a4
Jim Clark <slimslenderslacks@gmail.com>
2020-06-18 19:15:07 -0700

    change body

--- dev/user.cljs
+++ dev/user.cljs
@@ -4,9 +4,57 @@
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (comment
+
+ (require 'atomist.local-runner)
  (enable-console-print!)
- (atomist.local-runner/set-env :prod)
- (-> (atomist.local-runner/fake-push "T095SFFBK" "atomisthq" "bot-service" "master")
-     (assoc-in [:configuration] {:name "default"
-                                 :parameters [{:name "policy" :value "inPR"}]})
-     (atomist.local-runner/call-event-handler atomist.main/handler)))
+ (atomist.local-runner/set-env :prod-github-auth)
+
+ ;; should fail because of invalid config
+ (-> (atomist.local-runner/fake-push "T29E48P34" "atomist-skills" "jrday-testing" "master")
+     (assoc :configuration {:name "default"
+                            :parameters [{:name "fix" :value "inPR"}
+                                         {:name "config" :value ":cljfmt {:indents ^:replace {#\".*\" [[:inner 0]]}}"}]})
+     (atomist.local-runner/call-event-handler handler))
+ (-> (atomist.local-runner/fake-push "T29E48P34" "atomist-skills" "jrday-testing" "master")
+     (assoc :configuration {:name "default"
+                            :parameters [{:name "fix" :value "inPR"}
+                                         {:name "config" :value "{:indents ^:replace {#\".*\" [[:inner 0]]}}"}]})
+     (atomist.local-runner/call-event-handler handler))
+ (-> (atomist.local-runner/fake-push "T29E48P34" "atomist-skills" "jrday-testing" "master")
+     (assoc :configuration {:name "default"
+                            :parameters [{:name "fix" :value "inPR"}
+                                         {:name "config" :value "{org.me/foo [[:inner 0]]}"}]})
+     (atomist.local-runner/call-event-handler handler))
+
+ ;; should be a straight Commit
+ (-> (atomist.local-runner/fake-push "AEIB5886C" "slimslender" "clj1" "master")
+     (assoc :configuration {:name "default"
+                            :parameters [{:name "fix" :value "inPR"}
+                                         #_{:name "config" :value "{:indents {org.me/foo [[:inner 1]]}}"}]})
+     (atomist.local-runner/call-event-handler handler))
+
+ (-> (atomist.local-runner/fake-push "AEIB5886C" "slimslender" "clj1" "master")
+     (assoc :configuration {:name "default"
+                            :parameters [{:name "fix" :value "onDefaultBranch"}
+                                         #_{:name "config" :value "{:indents {org.me/foo [[:inner 1]]}}"}]})
+     (atomist.local-runner/call-event-handler handler))
+
+ ;; nothing to do not on default branch
+ (-> (atomist.local-runner/fake-push "AEIB5886C" "slimslender" "clj1" "slimslenderslacks-patch-1")
+     (assoc :configuration {:name "default"
+                            :parameters [{:name "fix" :value "onDefaultBranch"}
+                                         {:name "config" :value "{:indents {org.me/foo [[:inner 1]]}}"}]})
+     (atomist.local-runner/call-event-handler handler))
+
+ ;; something to do on
+ (-> (atomist.local-runner/fake-push "AEIB5886C" "slimslender" "clj1" "slimslenderslacks-patch-1")
+     (assoc :configuration {:name "default"
+                            :parameters [{:name "fix" :value "onBranch"}
+                                         {:name "config" :value "{:indents {org.me/foo [[:inner 1]]}}"}]})
+     (atomist.local-runner/call-event-handler handler))
+
+ (-> (atomist.local-runner/fake-push "AEIB5886C" "slimslender" "clj1" "slimslenderslacks-patch-1")
+     (assoc :configuration {:name "default"
+                            :parameters [{:name "fix" :value "inPROnDefaultBranch"}
+                                         #_{:name "config" :value "{:indents {org.me/foo [[:inner 1]]}}"}]})
+     (atomist.local-runner/call-event-handler handler)))
--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -42,7 +42,9 @@
                            :atomist.gitflows/configuration
                            {:branch (gstring/format "cljfmt-%s" (-> request :ref :branch))
                             :target-branch (-> request :ref :branch)
-                            :body (str "Configuration that triggered this change:\n" (:configuration request))
+                            :body (gstring/format
+                                   "running [cljfmt fix](https://github.com/weavejester/cljfmt) with configuration %s"
+                                   (-> request :configuration :name))
                             :title "cljfmt fix"
                             :type :in-pr})))
 
@@ -56,7 +58,7 @@
               (is-default-branch? request)))
             (<! (handler (assoc request
                            :atomist.gitflows/configuration
-                           {:message "cljfmt fix"
+                           {:message "running cljfmt fix"
                             :type :commit-then-push})))
 
             :else
@@ -104,58 +106,4 @@
                                                               :else
                                                               "handled Push successfully"))))})))
 
-(comment
 
- (require 'atomist.local-runner)
- (enable-console-print!)
- (atomist.local-runner/set-env :prod-github-auth)
-
- ;; should fail because of invalid config
- (-> (atomist.local-runner/fake-push "T29E48P34" "atomist-skills" "jrday-testing" "master")
-     (assoc :configuration {:name "default"
-                            :parameters [{:name "fix" :value "inPR"}
-                                         {:name "config" :value ":cljfmt {:indents ^:replace {#\".*\" [[:inner 0]]}}"}]})
-     (atomist.local-runner/call-event-handler handler))
- (-> (atomist.local-runner/fake-push "T29E48P34" "atomist-skills" "jrday-testing" "master")
-     (assoc :configuration {:name "default"
-                            :parameters [{:name "fix" :value "inPR"}
-                                         {:name "config" :value "{:indents ^:replace {#\".*\" [[:inner 0]]}}"}]})
-     (atomist.local-runner/call-event-handler handler))
- (-> (atomist.local-runner/fake-push "T29E48P34" "atomist-skills" "jrday-testing" "master")
-     (assoc :configuration {:name "default"
-                            :parameters [{:name "fix" :value "inPR"}
-                                         {:name "config" :value "{org.me/foo [[:inner 0]]}"}]})
-     (atomist.local-runner/call-event-handler handler))
-
- ;; should be a straight Commit
- (-> (atomist.local-runner/fake-push "AEIB5886C" "slimslender" "clj1" "master")
-     (assoc :configuration {:name "default"
-                            :parameters [{:name "fix" :value "inPR"}
-                                         #_{:name "config" :value "{:indents {org.me/foo [[:inner 1]]}}"}]})
-     (atomist.local-runner/call-event-handler handler))
-
- (-> (atomist.local-runner/fake-push "AEIB5886C" "slimslender" "clj1" "master")
-     (assoc :configuration {:name "default"
-                            :parameters [{:name "fix" :value "onDefaultBranch"}
-                                         #_{:name "config" :value "{:indents {org.me/foo [[:inner 1]]}}"}]})
-     (atomist.local-runner/call-event-handler handler))
-
- ;; nothing to do not on default branch
- (-> (atomist.local-runner/fake-push "AEIB5886C" "slimslender" "clj1" "slimslenderslacks-patch-1")
-     (assoc :configuration {:name "default"
-                            :parameters [{:name "fix" :value "onDefaultBranch"}
-                                         {:name "config" :value "{:indents {org.me/foo [[:inner 1]]}}"}]})
-     (atomist.local-runner/call-event-handler handler))
-
- ;; something to do on
- (-> (atomist.local-runner/fake-push "AEIB5886C" "slimslender" "clj1" "slimslenderslacks-patch-1")
-     (assoc :configuration {:name "default"
-                            :parameters [{:name "fix" :value "onBranch"}
-                                         {:name "config" :value "{:indents {org.me/foo [[:inner 1]]}}"}]})
-     (atomist.local-runner/call-event-handler handler))
-
- (-> (atomist.local-runner/fake-push "AEIB5886C" "slimslender" "clj1" "slimslenderslacks-patch-1")
-     (assoc :configuration {:name "default"
-                            :parameters [{:name "fix" :value "inPROnDefaultBranch"}
-                                         #_{:name "config" :value "{:indents {org.me/foo [[:inner 1]]}}"}]})
-     (atomist.local-runner/call-event-handler handler)))
